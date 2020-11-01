d7df2de21dac57d5b2eb5d48bc9d35a6c9828a5d
millywayne <achiengmillicent35@gmail.com>
2020-11-01 18:16:22 +0300

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quotes/quotes.component';
+import { QuoteComponent, QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuotesDetailComponent
+    QuotesDetailComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
